da817c1f35e84be070e5a52517b851dc0cd1c5c6
Sam Lew <40772654+samlew@users.noreply.github.com>
2025-06-02 22:18:07 -0400

    add logic to retrieve computer choice randomly from options

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('Hello world');
+const gameOptions = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+  return gameOptions[Math.floor(Math.random() * 3)];
+}
+
+console.log(getComputerChoice());
