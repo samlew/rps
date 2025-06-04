70906f05688cc8151969c39b1e4dc7ab129548d1
Sam Lew <40772654+samlew@users.noreply.github.com>
2025-06-03 22:53:38 -0400

    initialize player scores at 0

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const gameOptions = ["rock", "paper", "scissors"];
 
+let computerScore = 0;
+let humanScore = 0;
 
 function getComputerChoice() {
   let gameChoice = gameOptions[Math.floor(Math.random() * 3)];
@@ -10,6 +12,12 @@ function getHumanChoice() {
   let humanChoice = prompt("Rock, paper, or scissors?");
   return humanChoice;
 }
+// testing adding to human score
+// function addHumanScore() {
+//   humanScore += 99;
+//   return humanScore;
+// }
 
+// console.log(addHumanScore()); -- testing the add functionality
 console.log(getComputerChoice());
 console.log(getHumanChoice());
