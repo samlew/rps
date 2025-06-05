8d2113718d1830707d3af9266b6c90ecdbf3bd96
Sam Lew <40772654+samlew@users.noreply.github.com>
2025-06-05 07:31:47 -0400

    add options that beat other options, normalize choices, iterate on tie logic

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const gameOptions = ["rock", "paper", "scissors"];
+const beatOptions = beatOptions = ["paper", "scissors", "rock"];
 
 let computerScore = 0;
 let humanScore = 0;
 
 function getComputerChoice() {
-  let gameChoice = gameOptions[Math.floor(Math.random() * 3)];
+  let gameChoice = gameOptions[Math.floor(Math.random() * 3)].toUpperCase();
   return gameChoice;
 }
 
 function getHumanChoice() {
-  let humanChoice = prompt("Rock, paper, or scissors?");
+  let humanChoice = prompt("Rock, paper, or scissors?").toUpperCase();
   return humanChoice;
+}
+
+function playRound(humanChoice, computerChoice) {
+  if(humanChoice === computerChoice) {
+    console.log(`Both players chose ${humanChoice}. It's a tie.`);
+  }
+  else if(humanChoice)
+
 }
 // testing adding to human score
 // function addHumanScore() {
